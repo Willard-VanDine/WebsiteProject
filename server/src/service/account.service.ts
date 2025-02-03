7c7d71e7cb7d1e85d9f5c15c8e5ded2797065ef5
O-Brob <broborn@student.chalmers.se>
2025-02-03 20:43:29 +0100

    Very small change by changing json keys to not be written as strings. Apparently this was fine and allows vscode to highlight it properly.

--- server/src/service/account.service.ts
+++ server/src/service/account.service.ts
@@ -2,9 +2,9 @@ import { Account } from "../model/account.interface";
 
 export class AccountService {
     private acc : Account = {
-        "username" : "", 
-        "accountWins" : 0, 
-        "accountLosses" : 0
+        username : "", 
+        accountWins : 0, 
+        accountLosses : 0
     };
     
 }
