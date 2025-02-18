1538c344b75562cebfe06c7a058ea041711b1b74
O-Brob <broborn@student.chalmers.se>
2025-02-18 17:06:03 +0100

    Created test file for gameboard. Updated gameboard with JS to display round results. Fixed some broken imports.
    Co-authored-by: A-Azi-z <azizal@student.chalmers.se>
    Co-authored-by: Willard-VanDine <vesanen@chalmers.se>
    Co-authored-by: Suheib-Shahin <suhaib.syr@gmail.com>

--- client/src/Gameboard.tsx
+++ client/src/Gameboard.tsx
@@ -1,47 +1,64 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Stack from 'react-bootstrap/esm/Stack';
 import {Choice} from "../../server/src/model/choices.enum";
-import { makeMove, startGame } from './api';
+import {Gamestate} from "../../server/src/model/gamestate.interface"
+import { getGameScore, makeMove, startGame } from './api';
 import { Result } from '../../server/src/model/result.interface';
 //import Container from 'react-bootstrap/Container';
 //import Row from 'react-bootstrap/Row';
 //import Col from 'react-bootstrap/Col';
 
-const GameBoard = () => {
+const GameBoard = () => { // TODO: MAKE THIS PAGE SHOW CURRENT GAME SCORE. MAKE "START GAME" BUTTON RESET GAME. 
   return (
     // container class ensures consistency between pages 
     <div className="container" style={{height: "calc(100vh - 4rem - 8px)"}}>
       <div className=" d-flex flex-column justify-content-between" style={{ height: "100%" }}>
-        <div className="gameboard rounder"></div>
+        <div className="gameboard rounder">
+          <p id="GameroundResultText" data-testid="GameroundResultText">Make a choice to play!</p>
+        </div>
 
         <div className="container h-25" >
           <div className="row rounder buttoncontainer d-flex justify-content-around align-items-center">
             <div className="col-1"></div> 
 
             <button type="button" className="btn btn-primary btn-lg col-2" onClick={async () => {
-              await startGame();
+              
               const rock : Choice = Choice.Rock;
               const result : Result | undefined = await makeMove(rock);
               if (result === undefined){
                 console.log("Result undefined after choosing Rock");
                 return;
               }
-              console.log(result.result); // TODO: Actual logic
+              
+              const gameroundResultText = document.getElementById("GameroundResultText");
+              if(gameroundResultText !== null){
+                if(result.result === 1)
+                  gameroundResultText.innerHTML = "You Win!";
+                else if(result.result === 0)
+                  gameroundResultText.innerHTML = "Draw!";
+                else if(result.result === -1)
+                  gameroundResultText.innerHTML = "You Lose!";
+              }
 
             }}>Rock</button>
             <div className="col-2"></div>
 
             <button type="button" className="btn btn-primary btn-lg col-2" onClick={async () => {
-
+              
               const paper : Choice = Choice.Paper;
               const result : Result | undefined = await makeMove(paper);
               if (result === undefined){
                 console.log("Result undefined after choosing Paper");
                 return;
               }
-              console.log(result.result); // TODO: Actual logic
+              const gameroundResultText = document.getElementById("GameroundResultText");
+              if(gameroundResultText !== null){
+                if(result.result === 1)
+                  gameroundResultText.innerHTML = "You Win!";
+                else if(result.result === 0)
+                  gameroundResultText.innerHTML = "Draw!";
+                else if(result.result === -1)
+                  gameroundResultText.innerHTML = "You Lose!";
+              }
 
             }}>Paper</button>
             <div className="col-2"></div>
@@ -54,7 +71,15 @@ const GameBoard = () => {
                 console.log("Result undefined after choosing Scissor");
                 return;
               }
-              console.log(result.result); // TODO: Actual logic
+              const gameroundResultText = document.getElementById("GameroundResultText");
+              if(gameroundResultText !== null){
+                if(result.result === 1)
+                  gameroundResultText.innerHTML = "You Win!";
+                else if(result.result === 0)
+                  gameroundResultText.innerHTML = "Draw!";
+                else if(result.result === -1)
+                  gameroundResultText.innerHTML = "You Lose!";
+              }
 
             }}>Scissor</button>
             <div className="col-1"></div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from './header.tsx'
+import Header from './Header.tsx'
 import App from './App.tsx'
 import './index.css'
-import Footer from './footer.tsx'
+import Footer from './Footer.tsx'
 import Content from './Content.tsx'
 
 createRoot(document.getElementById('root')!).render(
